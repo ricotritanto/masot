32bca38b1a3eab7aacd1cc0df90842156611b432
ricotritanto <rico.tritanto89@gmail.com>
2019-01-28 15:21:23 +0700

    layout

---
+++ application/views/layout/head.php
@@ -0,0 +1,97 @@
+      
+        <div id="preloader">
+            <div class="loader absolute-center">
+                <div class="loader__box"><b class="top"></b></div>
+                <div class="loader__box"><b class="top"></b></div>
+                <div class="loader__box"><b class="top"></b></div>
+            </div>
+        </div>
+       
+        <!--================ Frist hader Area =================-->
+        <div class="first_header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="header_contact_details">
+                            <a href="#"><i class="fa fa-phone"></i>+1 (168) 314 5016</a>
+                            <a href="#"><i class="fa fa-envelope-o"></i>+1 (168) 314 5016</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="event_btn_inner">
+                            <!-- <a class="event_btn" href="table.html"><i class="fa fa-table" aria-hidden="true"></i>Book a Table</a>
+                            <a class="event_btn" href="event.html"><i class="fa fa-calendar" aria-hidden="true"></i>Book an Event</a> -->
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="header_social">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--================End Footer Area =================-->
+        
+        <!--================End Footer Area =================-->
+        <header class="main_menu_area">
+            <nav class="navbar navbar-default">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#"><img src="<?php echo base_url();?>assets/front/redcayenne/img/logo-1.png" alt=""></a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="index.html">Home</a></li>
+                            <li class="dropdown submenu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About US <!-- <i class="fa fa-angle-down" aria-hidden="true"></i> --></a>
+                               <!--  <ul class="dropdown-menu">
+                                    <li><a href="about-us.html">About Us</a></li>
+                                    <li><a href="about-us2.html">About Us2</a></li>
+                                </ul> -->
+                            </li>
+                            <li class="dropdown submenu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Produts <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="menu-grid.html">Menu Grid</a></li>
+                                    <li><a href="menu-list.html">Menu List</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown submenu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Brands <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="blog.html">Blog</a></li>
+                                    <li><a href="blog-gallery.html">Blog Gallery</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="gallery.html">Gallery</a></li>
+                            <li class="dropdown submenu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="event.html">Event</a></li>
+                                    <li><a href="table.html">Table</a></li>
+                                </ul>
+                            </li>                           
+                            <li><a href="contact.html">Contact US</a></li>
+                            <li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        </header>
+        <!--================End Footer Area =================-->
